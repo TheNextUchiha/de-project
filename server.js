0e04ec55bebb727415f590dbba8d6c68ec432cab
Farzan <farzanpira3@gmail.com>
2020-04-04 14:33:40 +0530

    Added Favicon

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 //cmd for Heroku & gitbash for Git and Github
-// require('dotenv').config();
 
 const express = require('express');
-const hbs = require('hbs');
-const _ = require('lodash');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require('passport');
@@ -11,12 +8,10 @@ const passport = require('passport');
 const {mongoose} = require('./server/db/mongoose');
 const {User} = require('./server/models/user');
 
-
+// Requiring Passport-config
 require('./passport-config')(passport);
 
 const index = require('./routes/index');
-// const users = require('./routes/users');
-// const auth = require('./routes/auth')(passport);
 
 var app = express();
 
@@ -24,11 +19,12 @@ var app = express();
 app.set('view engine','hbs');
 
 // ----> Express Middle-wares <-----
-app.use(express.urlencoded({extended: false})); // To Parse URL data
-app.use(express.json());                        // To Parse JSON data
-app.use(cookieParser());                        // To Parse Cookie data
-app.use(express.static(__dirname + '/views'));  // To include static HTML pages
-app.use(session({                               // Session Config
+app.use(favicon(__dirname + 'public/icons/favicon.ico'));   // To serve Favicon to the client
+app.use(express.urlencoded({extended: false}));             // To Parse URL data
+app.use(express.json());                                    // To Parse JSON data
+app.use(cookieParser());                                    // To Parse Cookie data
+app.use(express.static(__dirname + '/views'));              // To include static HTML pages
+app.use(session({                                           // Session Config
     secret: 'mysecret',
     saveUninitialized: false,
     resave: false
@@ -38,7 +34,7 @@ app.use(session({                               // Session Config
 app.use(passport.initialize());     // Passport Initialization
 app.use(passport.session());        // Passport Session Management
 
-// Routes
+// -----> Routes <-----
 app.use('/', index);
 
 /*
